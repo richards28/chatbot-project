d3595d5e702d97b70cdda708963f2ac8a3d5bcf8
richards28 <richirocks96@gmail.com>
2025-07-14 10:19:10 +0530

    Update dynamodb.tf

--- dynamodb/dynamodb.tf
+++ dynamodb/dynamodb.tf
@@ -1,1 +1,21 @@
+resource "aws_dynamodb_table" "faq" {
+  name           = "${var.project_prefix}_faq"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "Intent"
 
+  attribute {
+    name = "Intent"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "fallback_logs" {
+  name           = "${var.project_prefix}_unanswered"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "Timestamp"
+
+  attribute {
+    name = "Timestamp"
+    type = "S"
+  }
+}
