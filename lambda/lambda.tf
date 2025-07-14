2eb5550627e8b81ee5cf489c319eb35e7530cbab
richards28 <richirocks96@gmail.com>
2025-07-14 10:19:36 +0530

    Update lambda.tf

--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -1,1 +1,17 @@
+resource "aws_lambda_function" "faq_handler" {
+  filename         = "lambda/function.zip"
+  function_name    = "${var.project_prefix}_faq_handler"
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
+  source_code_hash = filebase64sha256("lambda/function.zip")
+  timeout          = 10
 
+  environment {
+    variables = {
+      FAQ_TABLE     = aws_dynamodb_table.faq.name
+      FALLBACK_TABLE = aws_dynamodb_table.fallback_logs.name
+      SUPPORT_EMAIL = "support@example.com"
+    }
+  }
+}
