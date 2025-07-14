5944b8de9db5681e8c27e869228dc745930bca80
richards28 <richirocks96@gmail.com>
2025-07-14 10:19:55 +0530

    Update iam.tf

--- iam/iam.tf
+++ iam/iam.tf
@@ -1,1 +1,48 @@
+resource "aws_iam_role" "lambda_exec" {
+  name = "${var.project_prefix}_lambda_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  role = aws_iam_role.lambda_exec.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem"
+        ],
+        Effect = "Allow",
+        Resource = [
+          aws_dynamodb_table.faq.arn,
+          aws_dynamodb_table.fallback_logs.arn
+        ]
+      },
+      {
+        Action = "ses:SendEmail",
+        Effect = "Allow",
+        Resource = "*"
+      },
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
+}
 
